26c8e1e54f677fadd34c7661be455342a0fd81f1
Jonhi7139 <125214537+Jonhi7139@users.noreply.github.com>
2026-08-18 15:25:43 +0100

    Add Serializer class header file

--- Microcontroller/src/Serializer.h
+++ Microcontroller/src/Serializer.h
@@ -1,1 +1,14 @@
+#ifndef Serializer_H
+#define Serializer_H
 
+#include "B13DOF.h"
+
+class Serializer
+{
+    public:
+        Serializer();
+        void serialSend(const environment& data);
+
+    private: 
+};
+#endif
